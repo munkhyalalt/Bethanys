edb981d6fe71d1ccb3ed943ae01eb35e119ae6b0
munkhyalalt <munkhyalalt.kh@gmail.com>
2017-03-28 17:31:04 +0800

    initial

---
+++ src/BethanysPieShop/Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace BethanysPieShop.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ src/BethanysPieShop/Models/Pie.cs
@@ -0,0 +1,12 @@
+﻿namespace BethanysPieShop.Models
+{
+    public class Pie
+    {
+        public int PieId { get; set; }
+        public string Name { get; set; }
+        public string ShortDescription { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
